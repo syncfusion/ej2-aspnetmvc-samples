9ab8e8a2e96fc5c153114d6a2abf8e3823a39770
BuildAutomation <buildautomation@syncfusion.com>
2025-08-08 06:14:10 +0000

    v30.2.4

---
+++ Controllers/Spreadsheet/RemoteDataBindingController.cs
@@ -0,0 +1,35 @@
+#region Copyright Syncfusion® Inc. 2001-2025.
+// Copyright Syncfusion® Inc. 2001-2025. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Syncfusion.EJ2.Spreadsheet;
+
+namespace EJ2MVCSampleBrowser.Controllers.Spreadsheet
+{
+    public partial class SpreadsheetController : Controller
+    {
+        public ActionResult RemoteDataBinding()
+        {
+            ViewData["FieldsOrder"] = new string[] { "OrderID", "CustomerID", "Freight", "ShipName", "ShipCity", "ShipCountry" };
+            return View();
+        }
+
+        public ActionResult RemoteDataOpen(OpenRequest openRequest)
+        {
+            return Content(Workbook.Open(openRequest));
+        }
+
+        public void RemoteDataSave(SaveSettings saveSettings)
+        {
+            Workbook.Save(saveSettings);
+        }
+    }
+}
